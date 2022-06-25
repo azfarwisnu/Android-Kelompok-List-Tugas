1f875bbdae6693d7a4aad3520da4f95fd6166a58
wisnuazfar <azfarwisnu@gmail.com>
2022-06-25 10:54:23 +0700

    push todo kelompok

---
+++ app/src/main/java/com/gwnbs/proyek8/ItemAdapter.java
@@ -0,0 +1,115 @@
+package com.gwnbs.proyek8;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormatSymbols;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ItemAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<ModelData> arrayList;
+
+    public ItemAdapter(Context context, ArrayList<ModelData> arrayList) {
+        super();
+        this.context = context;
+        this.arrayList = arrayList;
+    }
+
+    @Override
+    public int getCount() {
+        return this.arrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return arrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @SuppressLint({"ViewHolder", "InflateParams"})
+    @Override
+    public View getView(int position, View convertView, final ViewGroup parent) {
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        assert layoutInflater != null;
+        convertView = layoutInflater.inflate(R.layout.daftar_todo, null);
+        TextView titleTextView = convertView.findViewById(R.id.title);
+        TextView dateTextView = convertView.findViewById(R.id.dateTitle);
+        TextView timeTextView = convertView.findViewById(R.id.timeTitle);
+        final ImageView delImageView = convertView.findViewById(R.id.delete);
+        delImageView.setTag(position);
+
+        //Menghapus tugas dari database saat icon hapus di klik
+        delImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int pos = (int) v.getTag();
+                deleteItem(pos);
+            }
+        });
+
+        ModelData modelData = arrayList.get(position);
+        titleTextView.setText(modelData.getTitle());
+        String pattern = "yyyy-MM-dd";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        Date date = null;
+        try {
+            date = simpleDateFormat.parse(modelData.getDate());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        dateTextView.setText(date.toString());
+        //String newMonth = getMonth(monthOfYear + 1);
+        //tanggal.setText(dayOfMonth + "jedah " + newMonth);
+        //tanggal.setText(year+"-"+monthOfYear+"-"+dayOfMonth);
+        timeTextView.setText(modelData.getTime());
+        return convertView;
+    }
+
+    private String getMonth(int month) {
+        return new DateFormatSymbols().getMonths()[month - 1];
+    }
+
+    //Menghapus tugas dari listview
+    private void deleteItem(int position) {
+        deleteItemFromDb(arrayList.get(position).getId());
+        arrayList.remove(position);
+        notifyDataSetChanged();
+    }
+
+    //Menghapus tugas dari database
+    private void deleteItemFromDb(int id) {
+        DatabaseHelper databaseHelper = new DatabaseHelper(context);
+        try {
+            databaseHelper.deleteData(id);
+            toastMsg("Tugas di hapus");
+        } catch (Exception e) {
+            e.printStackTrace();
+            toastMsg("Oppss.. ada kesalahan saat menghapus");
+        }
+    }
+
+    //Metode pesan toast
+    private void toastMsg(String msg) {
+        Toast t = Toast.makeText(context, msg, Toast.LENGTH_SHORT);
+        t.setGravity(Gravity.CENTER,0,0);
+        t.show();
+    }
+
+}
